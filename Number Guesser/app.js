0340f48573ef7e9c9efc9f87ecd209347b0ef197
Simon Baumhauer <simon.baumhauer@hotmail.ch>
2022-07-29 15:57:14 +0200

    game logic added

--- Number Guesser/app.js
+++ Number Guesser/app.js
@@ -1,11 +1,11 @@
-/**
-    GAME FUNCTION:
- -  Player must guess a number between a min and max
- -  Player gets a certain amount of guesses
- -  Notify player of guesses remaining
- -  Notify the player of the correct answer if loose
- -  Let player choose to play again
- */
+/*
+GAME FUNCTION:
+- Player must guess a number between a min and max
+- Player gets a certain amount of guesses
+- Notify player of guesses remaining
+- Notify the player of the correct answer if loose
+- Let player choose to play again
+*/
 
 // Game values
 let min = 1,
@@ -14,20 +14,41 @@ let min = 1,
   guessesLeft = 3;
 
 // UI Elements
-const game = document.querySelector("game"),
+const game = document.querySelector("#game"),
   minNum = document.querySelector(".min-num"),
   maxNum = document.querySelector(".max-num"),
   guessBtn = document.querySelector("#guess-btn"),
   guessInput = document.querySelector("#guess-input"),
   message = document.querySelector(".message");
 
-// Assing UI min and max
+// Assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max;
 
 // Listen for guess
 guessBtn.addEventListener("click", function () {
-  //   let guess = parseInt(guessInput.value);
-  //   console.log(guess);
-  console.log("hello");
+  let guess = parseInt(guessInput.value);
+  console.log(guess);
+
+  // Validate
+  if (isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Please enter a number between ${min} and ${max}`, "red");
+  }
+
+  // Check if won
+  if (guess === winningNum) {
+    // Disable input
+    guessInput.disabled = true;
+    // Change border color
+    guessInput.style.borderColor = "green";
+    // Set Message
+    setMessage(`${winningNum} is correct, YOU WIN!`, "green");
+  } else {
+  }
 });
+
+// Set message
+function setMessage(msg, color) {
+  message.style.color = color;
+  message.textContent = msg;
+}
