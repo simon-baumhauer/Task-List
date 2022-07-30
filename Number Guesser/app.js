272ed0a5fa565540800dd1ba997e4a5398391bc7
Simon Baumhauer <simon.baumhauer@hotmail.ch>
2022-07-30 08:48:41 +0200

    gameover logic

--- Number Guesser/app.js
+++ Number Guesser/app.js
@@ -37,6 +37,7 @@ guessBtn.addEventListener("click", function () {
 
   // Check if won
   if (guess === winningNum) {
+    // Gameover - won
     // Disable input
     guessInput.disabled = true;
     // Change border color
@@ -44,6 +45,23 @@ guessBtn.addEventListener("click", function () {
     // Set Message
     setMessage(`${winningNum} is correct, YOU WIN!`, "green");
   } else {
+    // Wrong number
+    guessesLeft -= 1;
+
+    if (guessesLeft === 0) {
+      // Game over - lost
+      // Disable input
+      guessInput.disabled = true;
+      // Change border color
+      guessInput.style.borderColor = "green";
+      // Set Message
+      setMessage(
+        `Game Over, you lost. The correct number was ${winningNum}`,
+        "red"
+      );
+    } else {
+      // Game continues - answer wrong
+    }
   }
 });
 
