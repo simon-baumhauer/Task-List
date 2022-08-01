df1533fa504616f71c95dfd890ca786f1d36a474
Simon Baumhauer <simon.baumhauer@hotmail.ch>
2022-08-01 15:01:32 +0200

    add book object to book list

--- Book-List/app.js
+++ Book-List/app.js
@@ -1,4 +1,4 @@
-// Book Construtor
+// Book Constructor
 function Book(title, author, isbn) {
   this.title = title;
   this.author = author;
@@ -7,17 +7,46 @@ function Book(title, author, isbn) {
 // UI Constructor
 function UI() {}
 
+UI.prototype.addBookToList = function (book) {
+  const list = document.getElementById("book-list");
+  // Create tr element
+  const row = document.createElement("tr");
+  // insert cols
+  row.innerHTML = `
+  <td>${book.title}</td>
+  <td>${book.author}</td>
+  <td>${book.isbn}</td>
+  <td><a href="#" class="delete">x</a></td>`;
+
+  list.appendChild(row);
+};
+
+UI.prototype.clearFields = function () {
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("isbn").value = "";
+};
+
 // Event Listeners
 document.getElementById("book-form").addEventListener("submit", function (e) {
   // Get form values
-  const title = doucument.getElementById("title").value;
-  const author = doucument.getElementById("author").value;
-  const isbn = doucument.getElementById("isbn").value;
+  const title = document.getElementById("title").value,
+    author = document.getElementById("author").value,
+    isbn = document.getElementById("isbn").value;
 
   // Instantiate book
   const book = new Book(title, author, isbn);
 
-  console.log(book);
+  // Instantiate UI
+
+  const ui = new UI();
+
+  // Add boo to list
+
+  ui.addBookToList(book);
+
+  // Clear fields
+  ui.clearFields();
 
   e.preventDefault();
 });
