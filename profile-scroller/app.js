fe4b1ba59a631c5f9f07107d1772f790da6a2c6d
Simon Baumhauer <simon.baumhauer@hotmail.ch>
2022-08-16 11:40:10 +0200

    Module Pattern

--- profile-scroller/app.js
+++ profile-scroller/app.js
@@ -1,28 +1,28 @@
 const data = [
   {
-    name: 'John Doe',
+    name: "John Doe",
     age: 32,
-    gender: 'male',
-    lookingfor: 'female',
-    location: 'Boston MA',
-    image: 'https://randomuser.me/api/portraits/men/82.jpg'
+    gender: "male",
+    lookingfor: "female",
+    location: "Boston MA",
+    image: "https://randomuser.me/api/portraits/men/82.jpg",
   },
   {
-    name: 'Jen Smith',
+    name: "Jen Smith",
     age: 26,
-    gender: 'female',
-    lookingfor: 'male',
-    location: 'Miami FL',
-    image: 'https://randomuser.me/api/portraits/women/82.jpg'
+    gender: "female",
+    lookingfor: "male",
+    location: "Miami FL",
+    image: "https://randomuser.me/api/portraits/women/82.jpg",
   },
   {
-    name: 'William Johnson',
+    name: "William Johnson",
     age: 38,
-    gender: 'male',
-    lookingfor: 'female',
-    location: 'Lynn MA',
-    image: 'https://randomuser.me/api/portraits/men/83.jpg'
-  }
+    gender: "male",
+    lookingfor: "female",
+    location: "Lynn MA",
+    image: "https://randomuser.me/api/portraits/men/83.jpg",
+  },
 ];
 
 const profiles = profileIterator(data);
@@ -31,14 +31,14 @@ const profiles = profileIterator(data);
 nextProfile();
 
 // Next Event
-document.getElementById('next').addEventListener('click', nextProfile);
+document.getElementById("next").addEventListener("click", nextProfile);
 
 // Next Profile Display
 function nextProfile() {
   const currentProfile = profiles.next().value;
 
-  if(currentProfile !== undefined) {
-    document.getElementById('profileDisplay').innerHTML = `
+  if (currentProfile !== undefined) {
+    document.getElementById("profileDisplay").innerHTML = `
       <ul class="list-group">
         <li class="list-group-item">Name: ${currentProfile.name}</li>
         <li class="list-group-item">Age: ${currentProfile.age}</li>
@@ -47,7 +47,9 @@ function nextProfile() {
       </ul>
     `;
 
-    document.getElementById('imageDisplay').innerHTML = `<img src="${currentProfile.image}">`;
+    document.getElementById(
+      "imageDisplay"
+    ).innerHTML = `<img src="${currentProfile.image}">`;
   } else {
     // No more profiles
     window.location.reload();
@@ -59,10 +61,10 @@ function profileIterator(profiles) {
   let nextIndex = 0;
 
   return {
-    next: function() {
-      return nextIndex < profiles.length ? 
-      { value: profiles[nextIndex++], done: false } : 
-      { done: true }
-    }
+    next: function () {
+      return nextIndex < profiles.length
+        ? { value: profiles[nextIndex++], done: false }
+        : { done: true };
+    },
   };
 }
